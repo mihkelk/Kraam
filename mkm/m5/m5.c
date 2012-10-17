99b6fe9599766ca6b3a3fd22f164aaf5bc069342
Mihkel Kuslapuu <mihkelkuslapuu@gmail.com>
2012-10-17 12:37:16 +0300

    m5 - uus versioon, värvide ja vaateväljaga

---
+++ mkm/m5/m5.c
@@ -0,0 +1,140 @@
+/*
+ * m5.c
+ *
+ *  Created on: 17.10.2012
+ *      Author: mihkelk
+ */
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h> //värvide jaoks
+
+int x;
+int y;
+int koordinaadid[64][64][64];
+int suurus = 20;
+int m = 0;
+int px = 2;
+int py = 2;
+int jsuurus = 5;
+
+int toad( r) {
+	int x;
+	int y;
+
+	//üldise toa loomine
+
+	for (y = 0; y < (suurus + 1); y++) {
+		for (x = 0; x < (suurus + 1); x++) {
+
+			if ((y == suurus || y == 0) || (x == suurus || x == 0)) {
+				koordinaadid[r][y][x] = 1;
+			} else {
+				koordinaadid[r][y][x] = 0;
+			};
+		};
+	};
+
+	//vastava toa loomine
+	koordinaadid[10][20][10] = 2;
+	koordinaadid[10][4][6] = 0;
+
+	koordinaadid[20][4][6] = 1;
+	koordinaadid[20][0][11] = 2;
+
+	return 1;
+
+}
+
+int tuba1() {
+	px = 10;
+	py = 19;
+	return 1;
+}
+;
+int tuba2() { //tegelase teise toa otsa liigutamine
+	px = 11;
+	py = 1;
+	return 1;
+}
+;
+
+int main() {
+	int x;
+	int y;
+	//int koordinaadid[64][64][64];
+	//int suurus = 20;
+
+	int r = 10;
+
+	while ("TRUE") {
+
+		toad(r);
+
+		if (koordinaadid[10][py][px] == 2) {
+			r = 20;
+			tuba2();
+		};
+		if (koordinaadid[20][py][px] == 2) {
+			r = 10;
+			tuba1();
+		};
+
+//----------Klahvivajutuste-järgi-mängija-koordinaatide-muutmine----------//
+		char klahv = getch();
+
+		if (klahv == 'd' && (koordinaadid[r][py][px + 1] != 1)) {
+			px++;
+			koordinaadid[r][py][px - 1] = 0;
+		} else if (klahv == 'a' && (koordinaadid[r][py][px - 1] != 1)) {
+			px--;
+			koordinaadid[r][py][px + 1] = 0;
+		}
+		if (klahv == 'w' && (koordinaadid[r][py - 1][px] != 1)) {
+			py--;
+			koordinaadid[r][py + 1][px] = 0;
+		}
+		if (klahv == 's' && (koordinaadid[r][py + 1][px] != 1)) {
+			py++;
+			koordinaadid[r][py - 1][px] = 0;
+		}
+		toad(r);
+//------------------------------------------------------------------------//
+
+//-------------------------------joonistamine-----------------------------//
+		system("cls");
+
+		for (y = 0; y < (py + jsuurus + 1); y++) {
+			for (x = 0; x < (px + jsuurus + 1); x++) {
+				if (x == px && y == py) {
+					koordinaadid[r][y][x] = 3;
+				}
+				switch (koordinaadid[r][y][x]) {
+				case 0:
+					printf(" ");
+					break;
+				case 1:
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),
+							10);
+					printf("\333"); //ASCII koodid kaheksanddsüsteemis
+					break;
+				case 3:
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 3);
+					printf("\1");
+					break;
+
+				default:
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
+					printf("%d", koordinaadid[r][y][x]);
+					break;
+
+				};
+
+			};
+			printf("\n");
+		};
+//------------------------------------------------------------------------//
+	};
+	return 0;
+}
+
--- uus/uus/uus.c
+++ uus/uus/uus.c
@@ -8,12 +8,15 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main() {
 
 	//int h;
 
 	//for (h = 1; h < 900; h++){
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 6); //värv!!
+
 		printf("\333"); //kuuendsüsteemis
 	//};
 	getch();
