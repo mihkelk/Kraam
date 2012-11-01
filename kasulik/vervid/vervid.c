126e41595c5c68fc95d66d5de7cf515c5f5a6d8c
Mihkel Kuslapuu <mihkelkuslapuu@gmail.com>
2012-11-01 08:31:19 +0200

    Klahvivajutused. Tühik annab uued värvid. - taastab algsed värvid.

--- kasulik/vervid/vervid.c
+++ kasulik/vervid/vervid.c
@@ -10,36 +10,46 @@
 #include <stdlib.h>
 #include <windows.h>
 // Programm mis kirjutab välja värvide väärtusi
+char tser;
 int main() {
-
+	int kuni = 251;
 	int v;
 
-	for (v = 0; v < 251; v++) {
-		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), v); //värv!!
-		printf("%d", v);
-		printf("\n");
-	};
+	while (TRUE) {
+		for (v = 0; v < kuni; v++) {
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), v); //värv!!
+			printf("%d", v);
+			printf("\n");
+			//getch();
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0);
+		};
+		char tser = getch();
+		if (tser == '-') {
+			kuni = 251;
+			system("cls");
+		} else {
+			kuni = kuni + 251;
+		}
+	}
+		/*		printf("\333"); //kuuendsüsteemis
+		 SetCursorPos(50,20);
+		 printf("\333");
+		 //};
 
-	/*		printf("\333"); //kuuendsüsteemis
-	 SetCursorPos(50,20);
-	 printf("\333");
-	 //};
-
-	 char rida[128];
-	 FILE *sisse=fopen("tervitus.txt", "r");
-	 while(!feof(sisse)){
-	 fscanf(sisse,  "%s", rida);  //võtab ühe sõna
-	 fgets(rida, 128, sisse);
-	 printf("%s", rida);
-	 }
-	 fclose(sisse);
-	 return 1;
+		 char rida[128];
+		 FILE *sisse=fopen("tervitus.txt", "r");
+		 while(!feof(sisse)){
+		 fscanf(sisse,  "%s", rida);  //võtab ühe sõna
+		 fgets(rida, 128, sisse);
+		 printf("%s", rida);
+		 }
+		 fclose(sisse);
+		 return 1;
 
 
-	 */
-	getch();
+		 */
 
-	return 0;
-}
+		return 0;
+	}
 
 //http://www.mailsend-online.com/blog/setting-windows-console-text-colors-in-c.html
