40820ea800987f8c347b0396ed52db9915cf7fe8
mihkelk <mihkelk@192.168.210.53>
2012-11-02 10:02:58 +0200

    char int int char konversioon

--- kasulik/failist/fail.c
+++ kasulik/failist/fail.c
@@ -11,34 +11,80 @@
 #include <conio.h>
 
 int main(void) {
-	int x;
-	int y;
-	int r = 1;
-	int koordinaadid[80][80][80];
-char fnimi = '5';
-	FILE *fp = fopen("/fnimi", "r");
-	char asukoht = '0';
-	//üldise toa loomine
 
-	for (y = 0; (asukoht != '-'); y++) {
-		for (x = -3; asukoht !='\n'; x++) {
-			asukoht = fgetc(fp);
-		//	printf("%c", asukoht);
-			if (asukoht == '0') {
-				koordinaadid[r][y][x] = 0;
-			} else if (asukoht == '1') {
-				koordinaadid[r][y][x] = 1;
-			} else if (asukoht == '2') {
-				koordinaadid[r][y][x] = 2;
-			//} else {
-				//koordinaadid[r][y][x] = 0;
+	//char fnimi;
+//-------int chariks funktsioon-----//
+	/*	int c[]={1,4,6};
+	 char i;
+	 i = (c[0]) + 48;
+	 //  i = i + 48;
+	 printf("%c",i);
+	 ////////////////////////////////*/
+	//char fnimi[] = "5";
+	char heh;
+	char fnimi[];
+	int nfnimi = 5;
+	fnimi = nfnimi + 48;
 
-			};
-			printf("%d", koordinaadid[r][y][x]);
-		};
-		printf("\n");
-		asukoht = fgetc(fp);
-	};
+	FILE *fp = fopen(fnimi, "r");
+	heh = fgetc(fp);
+	printf("%c", heh);
+
+	nfnimi++;
+
+	 fnimi = (nfnimi + 48);
+
+		 fp = fopen(fnimi, "r");
+		heh = fgetc(fp);
+		printf("%c", heh);
+
+
+
+	/*	char c[]={'3',':','3'};
+	 int i;
+	 i = (c[0]);
+	 i = i - 48;
+	 printf("%d",i);
+	 */
+
+	/*int x;
+	 int y;
+	 int r = 1;
+	 int koordinaadid[80][80][80];
+	 char heh;
+	 char fnimi[] = "5";
+	 FILE *fp = fopen(fnimi, "r");
+	 heh =  fgetc(fp);
+	 printf("%c%", heh);
+	 numToChar()
+	 FILE *fp = fopen(fnimi, "r");
+	 heh =  fgetc(fp);
+	 printf("%c%", heh);
+	 */
+	//heh = );
+	//printf(heh);
+	/*char asukoht = '0';
+	 //üldise toa loomine
+
+	 for (y = 0; (asukoht != '-'); y++) {
+	 for (x = -3; asukoht !='\n'; x++) {
+	 asukoht = fgetc(fp);
+	 //	printf("%c", asukoht);
+	 if (asukoht == '0') {
+	 koordinaadid[r][y][x] = 0;
+	 } else if (asukoht == '1') {
+	 koordinaadid[r][y][x] = 1;
+	 } else if (asukoht == '2') {
+	 koordinaadid[r][y][x] = 2;
+	 //} else {
+	 //koordinaadid[r][y][x] = 0;
+
+	 };
+	 printf("%d", koordinaadid[r][y][x]);
+	 };
+	 printf("\n");
+	 asukoht = fgetc(fp);
+	 };*/
 	getch();
 	return 0;
 }
@@ -56,3 +102,12 @@ char fnimi = '5';
  return 1;
 
  }*/
+
+/* katkine
+ // itoa = integer to ascii
+ int e = 42;
+ char b[33];
+ char heh[33];
+ printf("%c", itoa(e,b,10));
+ //printf(heh);
+ */
