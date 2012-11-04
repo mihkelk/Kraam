fdb4b3be900556980caaa4284302f6ce1e450c22
Mihkel <mihkelkuslapuu@gmail.com>
2012-11-04 20:16:28 +0200

    m7- kasutab nüüdsest pdcurses

--- mkm/m7/m7.c
+++ mkm/m7/m7.c
@@ -5,18 +5,26 @@
  *      Author: Mihkel
  */
 
-#include <stdio.h>
-#include <conio.h>
+//#include <stdio.h>
+//#include <conio.h>
 #include <stdlib.h>
-#include <windows.h> //v‰rvide jaoks
+#include <windows.h> //
+#include <curses.h>
+//#include <cursesw.h>
+
+//char smiley = '\1'
+
 int x;
 int y;
 int z;
+int xpos = 0;
+int ypos = 0;
 int koordinaadid[100][100][100];
 int m = 0;
 int px = 30;
 int py = 30;
-int jsuurus = 10; //joonistatava vaatev‰lja suurus
+int jsuurus = 10; //joonistatava vaatev√§lja suurus
+int coord = 0;
 
 // Maailma loomine failist
 int maailm() {
@@ -24,7 +32,7 @@ int maailm() {
 
 	for (z = 49; z < 51; z++) {
 
-		cz[10] = itoa(z, cz, 10); //itoa(arv mida teisaldan, string millesse teisaldan, arvus¸steem)
+		cz[10] = itoa(z, cz, 10); //itoa(arv mida teisaldan, string millesse teisaldan, arvus√ºsteem)
 		//	printf("%s", cz); //[0], cz[1], cz[2]);
 
 		FILE *korrus = fopen(cz, "r");
@@ -36,7 +44,7 @@ int maailm() {
 
 				switch (casukoht) {
 				case '0':
-					koordinaadid[z][y][x] = 0; //t¸hjus
+					koordinaadid[z][y][x] = 0; //t√ºhjus
 					break;
 				case '1':
 					koordinaadid[z][y][x] = 1; //sein
@@ -45,7 +53,7 @@ int maailm() {
 					koordinaadid[z][y][x] = 2; //muru
 					break;
 				case '4':
-					koordinaadid[z][y][x] = 4; //pıısas
+					koordinaadid[z][y][x] = 4; //p√µ√µsas
 					break;
 				case '5':
 					koordinaadid[z][y][x] = 5; //trepp alla
@@ -54,7 +62,7 @@ int maailm() {
 					koordinaadid[z][y][x] = 6; //trepp yles
 					break;
 				case '7':
-					koordinaadid[z][y][x] = 7; //puitpırand
+					koordinaadid[z][y][x] = 7; //puitp√µrand
 					break;
 				default:
 					koordinaadid[z][y][x] = 9;
@@ -71,62 +79,56 @@ int maailm() {
 
 //-------------------------------joonistamine-----------------------------//
 int joonistamine() {
-	system("cls");
 
-	for (y = py - jsuurus; y < (py + jsuurus + 1); y++) {
-		for (x = px - jsuurus; x < (px + jsuurus + 1); x++) {
+	move(0, 0);
+
+	for (y = py - jsuurus; y < (py + jsuurus); y++) {
+
+		for (x = px - jsuurus; x < (px + jsuurus); x++) {
+
 			if (x == px && y == py) {
 				koordinaadid[z][y][x] = 3;
 			}
 			switch (koordinaadid[z][y][x]) {
 			case 0:
-				//SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 34);
-				printf(" ");
+				addch(ACS_BLOCK | COLOR_PAIR(6)); // s√ºmbolid laiendatud ascii tabelist pdcursesiga nimetused ACS_*
 				break;
 			case 1:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
-				printf("\333"); //ASCII koodid kaheksandds¸steemis
+				addch(ACS_BLOCK | COLOR_PAIR(1)); // V√§rve v√µetakse paaridena mis on defineeritud peafunktsioonis.
 				break;
 			case 2:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
-				printf("\261");
+				addch(ACS_CKBOARD | COLOR_PAIR(2));
 				break;
 			case 3:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 154);
-				printf("\1");
+				addch('\1' | COLOR_PAIR(4) | A_ALTCHARSET); // altcharset vajalik naerun√§eo n√§itamiseks
+				//refresh();
 				break;
 			case 4:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 294); //42
-				printf("\260");
+				addch(ACS_BOARD | COLOR_PAIR(5));
 				break;
 			case 5:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-				printf("v");
+				addch('v' | COLOR_PAIR(4));
 				break;
 			case 6:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-				printf("^");
+				addch('^' | COLOR_PAIR(4));
 				break;
 			case 7:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2680);
-				printf("\262");
-				// \334
+				addch(ACS_CKBOARD | COLOR_PAIR(7));
 				break;
-
 			default:
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 1);
-				printf("%d", koordinaadid[z][y][x]);
+				printw("%d", koordinaadid[z][y][x]);
 				break;
 			};
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0); // v‰rv tagasi mustaks, et ei tomuks jama taustav‰rvidega
-
 		};
-		printf("\n");
+		addch('\n');
 	};
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14);
-	printf("Kulda 50");
-	printf("   Elusid 50\n");
-	printf("Kıike 50");
+
+	//printw("Kulda 50");
+	//printw("   Elusid 50\n");
+	//printw("K√µike 50");
+
+	endwin(); //vajalik et ei tekiks jooni v√§rvide vahetusel, vabastab pdcurses-ile antud m√§lu
+
 	return 0;
 }
 //------------------------------------------------------------------------//
@@ -135,16 +137,35 @@ int joonistamine() {
 int main() {
 
 	maailm();
+	initscr();
+	//initscr();
+
+//http://pdcurses.sourceforge.net/doc/PDCurses.txt
+
+	resize_term(jsuurus * 3, jsuurus * 3); //konsooli suuruse muutmine
+
+	//PDCurses vervindus
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_WHITE);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(4, COLOR_RED, COLOR_WHITE);
+	init_pair(5, COLOR_GREEN, COLOR_YELLOW);
+	init_pair(6, COLOR_BLACK, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+
 	z = 50;
 	joonistamine();
 
 	int Q = 1;
-	int rs1 = 0; // salvestab ruudu kus m‰ngja seisab, et see hiljem taastada
-	int rs2 = 0; // salvestab teise ruudu kus m‰ngija seisis
+	int rs1 = 0; // salvestab ruudu kus m√§ngja seisab, et see hiljem taastada
+	int rs2 = 0; // salvestab teise ruudu kus m√§ngija seisis
 	int s1 = 1; // samm 1
 	int s2 = 0; // samm 2
+
 	while (Q == 1) {
-//----------Klahvivajutuste-j‰rgi-m‰ngija-koordinaatide-muutmine----------//
+//----------Klahvivajutuste-j√§rgi-m√§ngija-koordinaatide-muutmine----------//
+
 		char klahv = getch();
 
 		if (klahv == 'd' && (koordinaadid[z][py][px + 1] != 1)) {
@@ -235,14 +256,13 @@ int main() {
 
 			break;
 		}
-		joonistamine();
-		Sleep(10);
 
-	};
-	return 0;
+
+	joonistamine();
+
+};
+return 0;
 //------------------------------------------------------------------------//
 }
 //------------------------------------------------------------------------//
-
-// SetCursorPos(xpos,ypos); kursori paigutamine kuhugi, pole veel kasutusel
 
