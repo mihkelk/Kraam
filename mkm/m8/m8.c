d064b920dd71840ef4fe9d47800f2c8f9c49dc75
Mihkel Kuslapuu <mihkelkuslapuu@gmail.com>
2012-11-07 12:00:41 +0200

    Uus versioon, laeb otse charidena.

---
+++ mkm/m8/m8.c
@@ -0,0 +1,243 @@
+/*
+ * m8.c
+ *
+ *  Created on: 07.11.2012
+ *      Author: mihkelk
+ */
+
+//#include <stdio.h>
+//#include <conio.h>
+#include <stdlib.h>
+#include <windows.h> //
+#include <curses.h>
+//#include <dos.h>
+int x, y, z;
+char koordinaadid[100][500][500];
+int px = 248, py = 249; //mängija algkoordinaadid
+int jsuurus = 25; //joonistatava vaatevälja suurus
+BOOL WINAPI SetConsoleFont(HANDLE hOutput, DWORD fontIndex);
+
+// Maailma loomine failist
+int maailm() {
+	char cz[10];
+
+	for (z = 49; z < 51; z++) {
+
+		cz[10] = itoa(z, cz, 10); //itoa(arv mida teisaldan, string millesse teisaldan, arvusüsteem)
+		//	printf("%s", cz); //[0], cz[1], cz[2]);
+
+		FILE *korrus = fopen(cz, "r");
+		char casukoht = '0';
+		for (y = 0; casukoht != '-'; y++) {
+			for (x = 0; casukoht != '\n'; x++) {
+				casukoht = fgetc(korrus);
+
+				koordinaadid[z][y][x] = casukoht;
+
+			}
+			casukoht = fgetc(korrus);
+		};
+
+	};
+
+	return 1;
+}
+
+//http://blogs.microsoft.co.il/blogs/pavely/archive/2009/07/23/changing-console-fonts.aspx
+
+//-------------------------------joonistamine-----------------------------//
+int joonistamine() {
+
+	curs_set(0); //peidab kursori
+	int reavahetus = -1;
+
+	for (y = py - jsuurus; y < (py + jsuurus); y++) {
+		reavahetus++;
+		move(reavahetus, 0);
+		for (x = px - jsuurus; x < (px + jsuurus); x++) {
+
+			if (x == px && y == py) {
+				koordinaadid[z][y][x] = '3';
+			}
+			switch (koordinaadid[z][y][x]) {
+			case '0':
+				addch(ACS_BLOCK | COLOR_PAIR(6)); // sümbolid laiendatud ascii tabelist pdcursesiga nimetused ACS_*
+				break;
+			case '1':
+				addch(ACS_BLOCK | COLOR_PAIR(1)); // Värve võetakse paaridena mis on defineeritud peafunktsioonis.
+				break;
+			case '2':
+				addch(ACS_CKBOARD | COLOR_PAIR(2));
+				break;
+			case '3':
+				addch('\1' | COLOR_PAIR(4) | A_ALTCHARSET); // altcharset vajalik naerunäeo näitamiseks
+				break;
+			case '4':
+				addch(ACS_BOARD | COLOR_PAIR(5));
+				break;
+			case '5':
+				addch('v' | COLOR_PAIR(4));
+				break;
+			case '6':
+				addch('^' | COLOR_PAIR(4));
+				break;
+			case '7':
+				addch(ACS_CKBOARD | COLOR_PAIR(7));
+				break;
+			default:
+				printw("%d", koordinaadid[z][y][x]);
+				break;
+
+			};
+
+		};
+
+	};
+
+	//printw("Kulda 50");
+	//printw("   Elusid 50\n");
+	//printw("Kõike 50");
+	SetConsoleFont(GetStdHandle(STD_OUTPUT_HANDLE), 2); // vahetab fondi suurust, vajalik kuna kaotab ära jooned suuremat vilkumist põhjustamatta
+	//move(0, 0);
+
+	return 0;
+}
+//------------------------------------------------------------------------//
+
+//-------------------------------peafunktsioon----------------------------//
+int main() {
+
+	maailm();
+
+	//BOOL WINAPI SetConsoleFont(HANDLE hOutput, DWORD fontIndex);    //
+	//SetConsoleFont(GetStdHandle(STD_OUTPUT_HANDLE), 2); // paneb fondi suuruse paika
+
+	initscr();
+	//raw();
+	//noecho();
+	//initscr();
+
+//http://pdcurses.sourceforge.net/doc/PDCurses.txt
+
+	resize_term(50, 50); //konsooli suuruse muutmine
+
+	//PDCurses vervindus
+	start_color();
+	init_pair(1, COLOR_WHITE, COLOR_WHITE);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(3, COLOR_RED, COLOR_BLACK);
+	init_pair(4, COLOR_RED, COLOR_WHITE);
+	init_pair(5, COLOR_GREEN, COLOR_YELLOW);
+	init_pair(6, COLOR_BLACK, COLOR_BLACK);
+	init_pair(7, COLOR_WHITE, COLOR_BLACK);
+
+	z = 50;
+	joonistamine();
+
+	BOOL lopp = FALSE;
+	char rs1 = '0'; // salvestab ruudu kus mängja seisab, et see hiljem taastada
+	char rs2 = '2'; // salvestab teise ruudu kus mängija seisis
+	int s1 = 1; // samm 1
+	int s2 = 0; // samm 2
+
+	while (lopp == FALSE) {
+//----------Klahvivajutuste-järgi-mängija-koordinaatide-muutmine----------//
+
+		char klahv = getch();
+
+		if (klahv == 'd' && (koordinaadid[z][py][px + 1] != '1')) {
+
+			if (s1 == 1) {
+				s2 = 0;
+				rs1 = koordinaadid[z][py][px + 1];
+				px++;
+				koordinaadid[z][py][px - 1] = rs2;
+				s1 = 0;
+			}
+			if (s2 == 1) {
+				rs2 = koordinaadid[z][py][px + 1];
+				px++;
+				koordinaadid[z][py][px - 1] = rs1;
+				s2 = 0;
+				s1 = 1;
+			}
+
+		} else if (klahv == 'a' && (koordinaadid[z][py][px - 1] != '1')) {
+			//rsalvestatud = koordinaadid[z][py][px + 1];
+			if (s1 == 1) {
+				s2 = 0;
+				rs1 = koordinaadid[z][py][px - 1];
+				px--;
+				koordinaadid[z][py][px + 1] = rs2;
+				s1 = 0;
+			}
+			if (s2 == 1) {
+				rs2 = koordinaadid[z][py][px - 1];
+				px--;
+				koordinaadid[z][py][px + 1] = rs1;
+				s2 = 0;
+				s1 = 1;
+			}
+			//koordinaadid[z][py][px + 3] = rsalvestatud;*/
+		} else if (klahv == 'w' && (koordinaadid[z][py - 1][px] != '1')) {
+			if (s1 == 1) {
+				s2 = 0;
+				rs1 = koordinaadid[z][py - 1][px];
+				py--;
+				koordinaadid[z][py + 1][px] = rs2;
+				s1 = 0;
+			}
+			if (s2 == 1) {
+				rs2 = koordinaadid[z][py - 1][px];
+				py--;
+				koordinaadid[z][py + 1][px] = rs1;
+				s2 = 0;
+				s1 = 1;
+			}
+
+		} else if (klahv == 's' && (koordinaadid[z][py + 1][px] != '1')) {
+
+			if (s1 == 1) {
+				s2 = 0;
+				rs1 = koordinaadid[z][py + 1][px];
+				py++;
+				koordinaadid[z][py - 1][px] = rs2;
+				s1 = 0;
+			}
+			if (s2 == 1) {
+				rs2 = koordinaadid[z][py + 1][px];
+				py++;
+				koordinaadid[z][py - 1][px] = rs1;
+				s2 = 0;
+				s1 = 1;
+			}
+
+		} else if (klahv == 'q') {
+			lopp = TRUE;
+		}
+		s2 = 1;
+
+		switch (koordinaadid[z][py][px]) {
+		case '5':
+			z--;
+			rs1 = '7';
+			rs2 = '7';
+			break;
+		case '6':
+			z++;
+			rs1 = '7';
+			rs2 = '7';
+
+			break;
+		}
+		refresh();         // Peab läbi viima mitu korda et jooned kustuks.
+		joonistamine();    //
+		refresh();         //
+		joonistamine();    //
+
+	};
+	return 0;
+//------------------------------------------------------------------------//
+}
+//------------------------------------------------------------------------//
+
